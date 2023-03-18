7604722427d67ae163a8b63ff6ebd6b3de1da6f4
Basith P <basithpofficial@gmail.com>
2023-03-18 17:00:51 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/bns/todo_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bns.todo_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ToDoApp());
+}
+
+class ToDoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ToDo App',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ToDo App'),
+      ),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
